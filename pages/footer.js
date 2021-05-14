694898b5b3653ab185409e7e2aa5b97f831d96a0
AlrightErin27 <erinmontybruce@gmail.com>
2021-05-14 16:51:54 -0700

    added button links to footer

--- pages/footer.js
+++ pages/footer.js
@@ -2,5 +2,13 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function footer() {
-  return <div className={styles.footer}></div>;
+  return (
+    <div className={styles.footer}>
+      <div className={styles.contactContainer}>
+        <button className={styles.contactButton}>Linked In</button>
+        <button className={styles.contactButton}>Email</button>
+        <button className={styles.contactButton}>General Assembly</button>
+      </div>
+    </div>
+  );
 }
