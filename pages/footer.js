d8cdf806921173c2fb35951ce3a164d0541c244a
AlrightErin27 <erinmontybruce@gmail.com>
2021-05-22 13:44:05 -0400

    added links in footer

--- pages/footer.js
+++ pages/footer.js
@@ -1,13 +1,27 @@
-import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function footer() {
   return (
     <div className={styles.footer}>
       <div className={styles.contactContainer}>
-        <button className={styles.contactButton}>Linked In</button>
-        <button className={styles.contactButton}>Email</button>
-        <button className={styles.contactButton}>General Assembly</button>
+        <button className={styles.contactButton}>
+          <Link href="https://www.linkedin.com/in/erin-van-brunt/">
+            Linked In
+          </Link>
+        </button>
+
+        <button className={styles.contactButton}>
+          <Link href="https://generalassemb.ly/education/software-engineering-immersive/seattle">
+            General Assembly
+          </Link>
+        </button>
+
+        <button className={styles.contactButton}>
+          <Link href="mailto:erinmontybruce@gmail.com">
+            erinmontybruce@gmail.com
+          </Link>
+        </button>
       </div>
     </div>
   );
