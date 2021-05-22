e65df662fdf377c5a26fa083250c5121b365314b
AlrightErin27 <erinmontybruce@gmail.com>
2021-05-22 12:47:30 -0400

    added project info buttons

--- pages/projects.js
+++ pages/projects.js
@@ -7,9 +7,13 @@ export default function projects() {
       <h3 className={styles.projects}>Projects:</h3>
 
       <button className={styles.projectsButton}>Q-TIP GAME</button>
+      <button className={styles.infoButton}>INFO</button>
       <button className={styles.projectsButton}>CASTERLY ROCK</button>
+      <button className={styles.infoButton}>INFO</button>
       <button className={styles.projectsButton}>PARLOR OF FORTUNE</button>
+      <button className={styles.infoButton}>INFO</button>
       <button className={styles.projectsButton}>ADVENTURE</button>
+      <button className={styles.infoButton}>INFO</button>
     </div>
   );
 }
