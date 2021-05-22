267209f2cdc458ab0475d4639452e5ddf98a59d2
AlrightErin27 <erinmontybruce@gmail.com>
2021-05-22 15:11:12 -0400

    added links to projects P4 NOT LOADING CORRECT

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ http://localhost:3000/
 Instantly deploy your Next.js site to a public URL with Vercel.
 
 npm run dev
+
+https://alrighterin27.github.io/
+https://gameofthrones-api-project.herokuapp.com/
+https://6060efd260636ea29c3b76c0--madames-parlor-of-fortune.netlify.app/
+https://adventurewanderingsoul.herokuapp.com/
--- pages/projects.js
+++ pages/projects.js
@@ -6,28 +6,42 @@ export default function projects() {
     <div className={styles.projectsContainer}>
       <h3 className={styles.projects}>Projects:</h3>
       {/* -----------------P1--------------------------- */}
-      <button className={styles.projectsButton}>Q-TIP GAME</button>
+      <button className={styles.projectsButton}>
+        <Link href="https://alrighterin27.github.io/">Q-TIP GAME</Link>
+      </button>
       <button className={styles.infoButton}>
         <Link href="https://github.com/AlrightErin27/P1-Sleep-Game/blob/main/README.md">
           INFO
         </Link>
       </button>
       {/* -----------------P2--------------------------- */}
-      <button className={styles.projectsButton}>CASTERLY ROCK</button>
+      <button className={styles.projectsButton}>
+        <Link href="https://gameofthrones-api-project.herokuapp.com/">
+          CASTERLY ROCK
+        </Link>
+      </button>
       <button className={styles.infoButton}>
         <Link href="https://github.com/AlrightErin27/GameOfThronesAPI-P2/blob/main/README.md">
           INFO
         </Link>
       </button>
       {/* -----------------P3--------------------------- */}
-      <button className={styles.projectsButton}>PARLOR OF FORTUNE</button>
+      <button className={styles.projectsButton}>
+        <Link href="https://6060efd260636ea29c3b76c0--madames-parlor-of-fortune.netlify.app/">
+          PARLOR OF FORTUNE
+        </Link>
+      </button>
       <button className={styles.infoButton}>
         <Link href="https://github.com/AlrightErin27/fortuneteller_client/blob/main/FortuneREADME.md">
           INFO
         </Link>
       </button>
       {/* -----------------P4--------------------------- */}
-      <button className={styles.projectsButton}>ADVENTURE</button>
+      <button className={styles.projectsButton}>
+        <Link href="https://adventurewanderingsoul.herokuapp.com/">
+          ADVENTURE
+        </Link>
+      </button>
       <button className={styles.infoButton}>
         <Link href="https://github.com/AlrightErin27/p4_ChooseYouOwnAdventure/blob/main/README.md">
           INFO
