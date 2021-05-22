ce0a8401dbfe647549e49d1b78232753ca989c25
AlrightErin27 <erinmontybruce@gmail.com>
2021-05-22 13:19:12 -0400

    added jpg resume in link

---
+++ pages/paperResume.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function paperResume() {
+  return (
+    <div className={styles.paperResumeDisplay}>
+      <Image
+        className={styles.paperResume}
+        src="/ResumePort1.jpg"
+        alt="resume1"
+        height={1150}
+        width={950}
+      />
+      <Image
+        className={styles.paperResume}
+        src="/ResumePort2.jpg"
+        alt="resume2"
+        height={1150}
+        width={950}
+      />
+    </div>
+  );
+}
--- pages/resume.js
+++ pages/resume.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function resume() {
   return (
     <div className={styles.resumeContainer}>
-      <button className={styles.resumeButton}>RESUME</button>
+      <button className={styles.resumeButton}>
+        <Link href="/paperResume" rel="resume" className={styles.resumeLink}>
+          RESUME
+        </Link>
+      </button>
     </div>
   );
 }
